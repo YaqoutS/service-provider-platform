ca2f4e2c9cbf9c50945e7186340fb4ddb30c29eb
yaqout salameh <yaqoutsalameh@gmail.com>
2023-10-23 23:35:27 +0300

    Initial commit

---
+++ src/main/java/com/graduationproject/serviceproviderplatform/ServiceProviderPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.graduationproject.serviceproviderplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graduationproject/serviceproviderplatform/ServiceProviderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduationproject.serviceproviderplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
