672b1fbb06a8ce534f5b7585f6e204e96d904836
yaqout salameh <yaqoutsalameh@gmail.com>
2023-12-10 21:57:56 +0200

    Defining CommandLineRunner Bean to add initial data

--- src/main/java/com/graduationproject/serviceproviderplatform/ServiceProviderPlatformApplication.java
+++ src/main/java/com/graduationproject/serviceproviderplatform/ServiceProviderPlatformApplication.java
@@ -1,13 +1,70 @@
 package com.graduationproject.serviceproviderplatform;
 
+import com.graduationproject.serviceproviderplatform.model.*;
+import com.graduationproject.serviceproviderplatform.repository.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+import java.util.Arrays;
+import java.util.HashSet;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ServiceProviderPlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceProviderPlatformApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initData(CategoryRepository categoryRepository,
+                                      UserRepository userRepository,
+                                      EmployeeRepository employeeRepository,
+                                      RoleRepository roleRepository,
+                                      CustomerRepository customerRepository,
+                                      PasswordEncoder passwordEncoder) {
+        return args -> {
+            System.out.println("Inside the command line runner");
+
+            String secret = passwordEncoder.encode("123456");
+
+            Role customerRole = new Role("ROLE_CUSTOMER");
+            roleRepository.save(customerRole);
+            Role adminRole = new Role("ROLE_ADMIN");
+            roleRepository.save(adminRole);
+            Role employeeRole = new Role("ROLE_EMPLOYEE");
+            roleRepository.save(employeeRole);
+
+            Customer customer = new Customer("customer@gmail.com", "Customer Customer", secret, true);
+            customer.addRole(customerRole);
+            customer.setConfirmPassword(secret);
+            customerRepository.save(customer);
+
+            User admin = new User("admin@gmail.com", "Admin Admin", secret, true);
+            admin.addRole(adminRole);
+            admin.setConfirmPassword(secret);
+            userRepository.save(admin);
+
+            Employee employee = new Employee("employee@gmail.com", "Employee Employee", secret, true, true);
+            employee.addRole(employeeRole);
+            employee.setConfirmPassword(secret);
+            employeeRepository.save(employee);
+
+            System.out.println("Users and roles added successfully");
+
+            // Adding categories
+            Category category1 = new Category("Category 1");
+            Category category2 = new Category("Category 2");
+            Category category3 = new Category("Category 3");
+            categoryRepository.save(category1);
+            categoryRepository.save(category2);
+            categoryRepository.save(category3);
+            System.out.println("Categories added successfully");
+
+        };
     }
 
 }
