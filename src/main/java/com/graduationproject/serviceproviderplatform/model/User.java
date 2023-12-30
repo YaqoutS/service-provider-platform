4a882b8212e0f4df9bf2a03bf5cadadeae4cca5b
yaqout salameh <yaqoutsalameh@gmail.com>
2023-12-30 18:39:56 +0200

    Fixing NullPointerException by allowing serialization of password and confirmPassword fields (removing @JsonIgnore)

--- src/main/java/com/graduationproject/serviceproviderplatform/model/User.java
+++ src/main/java/com/graduationproject/serviceproviderplatform/model/User.java
@@ -1,21 +1,15 @@
 package com.graduationproject.serviceproviderplatform.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.graduationproject.serviceproviderplatform.model.validator.PasswordMatch;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
 @RequiredArgsConstructor
@@ -41,13 +35,9 @@ public class User {
 
     @NonNull
     @Column(length = 100)
-    @JsonIgnore
-    @ToString.Exclude
     private String password;
 
     @Transient
-    @JsonIgnore
-    @ToString.Exclude
     private String confirmPassword;
 
     @Past(message = "Date of birth must be in the past.")
@@ -90,37 +80,7 @@ public class User {
         }
     }
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-//    }
-
     public void addRole(Role role) {
         roles.add(role);
     }
-
-//    @Override
-//    public String getUsername() {
-//        return fullName;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
 }
